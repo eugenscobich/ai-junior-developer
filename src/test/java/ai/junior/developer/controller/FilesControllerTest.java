ec7320a855972a4ac5ba06a69d5e49f6ef2c36a4
Evgheni Scobici <evghenii.scobici@endava.com>
2025-04-25 17:29:59 +0300

    MDR-1023: Add authentication context to controller tests using @WithMockUser

--- src/test/java/ai/junior/developer/controller/FilesControllerTest.java
+++ src/test/java/ai/junior/developer/controller/FilesControllerTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,10 +15,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FilesController.class)
+@WithMockUser(username = "user", password = "password", roles = "USER")
 class FilesControllerTest {
 
     @Autowired
--- src/test/java/ai/junior/developer/controller/GitControllerTest.java
+++ src/test/java/ai/junior/developer/controller/GitControllerTest.java
@@ -1,22 +1,20 @@
 package ai.junior.developer.controller;
 
 import ai.junior.developer.service.GitService;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(GitController.class)
+@WithMockUser(username = "user", password = "password", roles = "USER")
 class GitControllerTest {
 
     @Autowired

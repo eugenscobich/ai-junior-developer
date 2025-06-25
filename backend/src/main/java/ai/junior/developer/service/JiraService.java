2b3c72d191454dd3d676a43200b310bbdcc83d9a
Evgheni Scobici <evghenii.scobici@endava.com>
2025-06-25 09:57:42 +0300

    Continue working

--- backend/src/main/java/ai/junior/developer/service/JiraService.java
+++ backend/src/main/java/ai/junior/developer/service/JiraService.java
@@ -100,8 +100,7 @@ public class JiraService {
 
     public static String getReplayCommentBody(String assistantId, String threadId) {
         return "Task was assigned to AI Junior Developer.\n"
-            + "Links:\n[AI Junior Developer Console](https://jknights.serveo.net/" + threadId + "/messages)\n"
-            + "\n[AI Junior Developer Console backup](https://jknights.eugenscobich.com/" + threadId + "/messages)\n"
+            + "Links:\n[AI Junior Developer Console](https://jknights.eugenscobich.com/" + threadId + "/messages)\n"
             + "[OpenAI Platform](https://platform.openai.com/playground/assistants?assistant=" + assistantId + "&thread=" + threadId + ")\n";
     }
 
